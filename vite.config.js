118822b0d0e6a9a788e7096ab11cead8a46dbca6
Willy <willy025896@gmail.com>
2024-11-21 10:05:10 +0800

    Chore: 更新 config

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,12 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/vue3-wigets/' : '/',
 });
