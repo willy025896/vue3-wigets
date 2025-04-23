7f291be68d8382e719b54aea7e994ad1b3f10c26
Willy <willy025896@gmail.com>
2025-04-23 13:41:52 +0800

    fix: 修正重整跳轉回原本頁面

--- src/main.js
+++ src/main.js
@@ -6,17 +6,17 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
-const redirect = sessionStorage.redirect;
-console.log(redirect);
-if (redirect) {
-  sessionStorage.removeItem('redirect');
-  router.replace(redirect);
-  console.log(redirect);
-}
-
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 
-app.mount('#app');
+router.isReady().then(() => {
+  const redirect = sessionStorage.redirect;
+  if (redirect) {
+    sessionStorage.removeItem('redirect');
+    router.replace(redirect);
+  }
+
+  app.mount('#app');
+});
